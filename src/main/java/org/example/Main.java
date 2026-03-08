c4a935bec176e0530409bc2057a9a21f0fe6512a
Njaka <hei.herinjaka@gmail.com>
2026-03-08 11:47:19 +0300

    feat: add enum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
 
-        try (Connection connection = DBConnection.getConnection()) {
-            System.out.println("Connection successful");
-        } catch (RuntimeException | SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        
+
 
     }
 }
---
+++ src/main/java/org/example/ModeleVoiture.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public enum ModeleVoiture {
+
+    GETZ, PRIDE, LACETTI
+
+}
